49df32f803b30450e9d3ff5b54444e3c40587166
adiletmaks <adilet970113@gmail.com>
2019-11-01 15:40:49 +0300

    Role model

---
+++ myProject/Data/BlogPlatformContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myProject.Data
+{
+    public class BlogPlatformContext : DbContext
+    {
+        public BlogPlatformContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
