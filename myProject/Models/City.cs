3cebc3aa726c20057905121bca96a2dcd7a00f7c
adiletmaks <adilet970113@gmail.com>
2019-11-16 04:35:52 +0600

    model implement IValidatableObject

--- myProject/Models/City.cs
+++ myProject/Models/City.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using myProject.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -7,7 +8,7 @@ using System.Web;
 
 namespace myProject.Models
 {
-    public class City
+    public class City : IValidatableObject
     {
         public uint Id { get; set; }
 
@@ -19,5 +20,32 @@ namespace myProject.Models
         public uint CountryId { get; set; }
 
         public Country Country { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            if (string.IsNullOrWhiteSpace(this.Name))
+            {
+                errors.Add(new ValidationResult("Enter city name!"));
+            }
+            if (BlackList().Contains(this.Name))
+            {
+                errors.Add(new ValidationResult("Enter other city name!"));
+            }
+            if (this.CountryId < 1)
+            {
+                errors.Add(new ValidationResult("Invalid city id!"));
+            }
+
+            return errors;
+        }
+
+        public List<String> BlackList() {
+            List<string> list = new List<string>(new string[] { "City1", "City2", "City3" });
+
+            return list;
+        }
+
     }
 }
