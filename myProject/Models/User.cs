89350976eac8a8dde3e196a377b69cab15a7b78d
adiletmaks <adilet970113@gmail.com>
2019-11-16 03:54:45 +0600

    added custom attribute validation

--- myProject/Models/User.cs
+++ myProject/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using myProject.Validations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -7,6 +8,7 @@ using System.Web;
 
 namespace myProject.Models
 {
+    [CheckPasswordStrength]
     public class User
     {
         public uint Id { get; set; }
---
+++ myProject/Validations/CheckPasswordStrengthAttribute.cs
@@ -0,0 +1,27 @@
+﻿using myProject.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myProject.Validations
+{
+    public class CheckPasswordStrengthAttribute : ValidationAttribute
+    {
+        public CheckPasswordStrengthAttribute()
+        {
+            ErrorMessage = "Password should not contains First name, last name!";
+        }
+        public override bool IsValid(object value)
+        {
+            User u = value as User;
+            if (u.Password.Contains(u.FirstName) || u.Password.Contains(u.LastName))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
